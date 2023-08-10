0e827fe194a016143f526016cc1359a8859ccf79
J3cks <jecks.pursuitscoding@gmail.com>
2023-08-10 14:32:39 +0800

    Added answers on 02 in the forked repo

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(word, iteration) {
+    let baseWord = '';
+    if (iteration >= 0) {
+        for (i = 0; i < iteration; i++) {
+            baseWord = word + baseWord;
+        }
+        
+    } else {
+        baseWord = 'ERROR'; 
+    }
+    let finalWord = baseWord;
+    return finalWord;
 };
+repeatString("hey", -1);
 
 // Do not edit below this line
 module.exports = repeatString;
